1af435bcdc5f6b7c7d5af7b5276805f2400d5d4d
jiumogaoao <394127821@qq.com>
2015-07-14 18:34:29 +0800

    change
    change

--- js/ppt.js
+++ js/ppt.js
@@ -13,14 +13,28 @@
 		}
 	app.up=function(){
 		if(nowpage<$(".page").length-1){
+			$(".page")[nowpage].find(".child").each(function(){
+				$(this).attr("class","child"+$(this).attr("outAn"))
+				});
 			nowpage++;
-			$("#roll").animate({"top":-1*$(window).height()*nowpage});
+			$("#roll").animate({"top":-1*$(window).height()*nowpage},function(){
+				$(".page")[nowpage].find(".child").each(function(){
+				$(this).attr("class","child"+$(this).attr("inAn")+$(this).attr("onAn"))
+				});
+				});
 			}
 		};
 	window.app.down=function(){
 		if(nowpage>0){
+			$(".page")[nowpage].find(".child").each(function(){
+				$(this).attr("class","child"+$(this).attr("outAn"))
+				});
 			nowpage--;
-			$("#roll").animate({"top":-1*$(window).height()*nowpage});
+			$("#roll").animate({"top":-1*$(window).height()*nowpage},function(){
+				$(".page")[nowpage].find(".child").each(function(){
+				$(this).attr("class","child"+$(this).attr("inAn")+$(this).attr("onAn"))
+				});
+				});
 			}
 		};
 	window.app.run=function(){
